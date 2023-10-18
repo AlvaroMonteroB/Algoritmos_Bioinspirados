64edde0c86f50c2106dfc9c34ef694f98102ba59
AlvaroMonteroB <alvaromonterob@outlook.com>
2023-10-18 00:38:22 -0600

    Using uniform

--- P1/Practica 1/main.py
+++ P1/Practica 1/main.py
@@ -74,7 +74,7 @@ class Pop():
         self.best_chrom=self.best_individual()
             
     def roulette(self):#Metodo de ruleta
-        prob=rd.random()
+        prob=rd.uniform(0,1)
         fitness=list()        
         for chrom in self.individuals:
             aux=0
@@ -128,12 +128,10 @@ class Pop():
                     parent1=self.individuals[pair1]
                     parent2=self.individuals[pair2]
                     band=0
-                    if rd.random()>.85:#Si el valor supera .85 no se cruzan
+                    if rd.uniform(0,1)>.85:#Si el valor supera .85 no se cruzan
                         new_generation.append(deepcopy(parent1)) #se pasan a la siguiente generacion
                         new_generation.append(deepcopy(parent2))
                         band=1
-                        break
-                    if band==1:
                         break
                     #hacer cruza, mutacion y escoger los mejores individuos
                     
@@ -142,26 +140,38 @@ class Pop():
                     prospectos=list() 
                     while True:
                         
+                        pos_hijos=[]
                         for k in range(self.genes):#Generar hijo
-                            aux_chr=[]#Cromosoma auxiliar
-                            if rd.random()>=.5:#Si es mayor a .5 elegimos padre 2
-                                aux_chr.append(deepcopy(parent2[k]))
+                            pos_hijos.append(rd.uniform(0,1))
+
+                        for k in range(len(pos_hijos)):
+                            aux_chr1=[]
+                            aux_chr2=[]
+                            if pos_hijos[k]>=.5:#Si es mayor a .5 elegimos padre 2
+                                aux_chr1.append(deepcopy(parent2[k]))
                             else:
-                                aux_chr.append(deepcopy(parent1[k]))
+                                aux_chr1.append(deepcopy(parent1[k]))
+                                
+                            if pos_hijos[k]>=.5:#El hermano es el complemento
+                                aux_chr2.append(deepcopy(parent1[k]))
+                            else:
+                                aux_chr2.append(deepcopy(parent2[k]))
                                 
                             #Mutacion    
-                        for mut in range(len(aux_chr)):
-                            if rd.random()<.1:#Si es menor a .1 mutamos
-                                aux_chr[mut].quantity=rd.randint(self.min[mut],self.max[mut])
+                        for mut in range(len(aux_chr1)):
+                            if rd.uniform(0,1)<.1:#Si es menor a .1 mutamos
+                                aux_chr1[mut].quantity=rd.randint(self.min[mut],self.max[mut])
+                        for mut in range(len(aux_chr2)):
+                            if rd.uniform(0,1)<.1:
+                                aux_chr2[mut].quantity=rd.randint(self.min[mut],self.max[mut])
                                         
                            #Checar condicion de peso    
-                        if chr_weight(aux_chr)>self.weight_max:#SI no se cumple la condicion de peso se vuelve a generar
+                        if chr_weight(aux_chr1)>self.weight_max and chr_weight(aux_chr2)>self.weight_max:#SI no se cumple la condicion de peso se vuelve a generar
                             continue#SI no cumple volvemos a generar
-                        elif 0<chr_weight(aux_chr)<=self.weight_max :#si cumple vamos a ponerlo en los hijos
-                            prospectos.append(deepcopy(aux_chr))
-                            cont+=1
-                        if cont==2:#SI ya se generaron los 2 hijos continuamos
-                            break  
+                        elif 0<chr_weight(aux_chr1)<=self.weight_max and 0<chr_weight(aux_chr2)<=self.weight_max :#si cumple vamos a ponerlo en los hijos
+                            prospectos.append(deepcopy(aux_chr1))
+                            prospectos.append(deepcopy(aux_chr2))
+                            break
                         
                     prospectos.append(parent1)
                     prospectos.append(parent2)
@@ -182,12 +192,22 @@ class Pop():
 
             self.individuals=new_generation.copy()#Formamos la nueva generacion 
             fitness_gen,generation_best=self.best_individual()
+            if i<2:
+                print("Generacion "+str(i))
+                for m in self.individuals:
+                    for n in m:
+                        print(str(n.quantity))
+                    print("\n")
+            
+            
+            
+            
             if fitness_gen>self.best_chrom[0]:
                 self.best_chrom=(fitness_gen,generation_best)
                 sin_mejora=0
             else:
                 sin_mejora+=1
-            if sin_mejora>10:
+            if sin_mejora>5:
                 break
             
                        
