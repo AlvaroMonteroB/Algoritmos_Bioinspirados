49dc058cc7b0e8ab2f8bbc60035f5c4b38b14d44
AlvaroMonteroB <montero.barraza.alvaro.david@gmail.com>
2024-01-08 23:42:16 -0600

    deap bien

--- P1/Practica 2/deap_main.py
+++ P1/Practica 2/deap_main.py
@@ -15,46 +15,36 @@ from deap import creator
 from deap import tools
 from deap import gp
 
-# Define new functions
-def protectedDiv(left, right):
-    try:
-        return left / right
-    except ZeroDivisionError:
-        return 1
 
-pset = gp.PrimitiveSet("MAIN", 1)
-pset.addPrimitive(operator.add, 2)
-pset.addPrimitive(operator.sub, 2)
-pset.addPrimitive(operator.mul, 2)
-pset.addPrimitive(protectedDiv, 2)
-pset.addPrimitive(operator.neg, 1)
-pset.addPrimitive(math.cos, 1)
-pset.addPrimitive(math.sin, 1)
+pset = gp.PrimitiveSet("MAIN", 2) #Numero de entradas
+pset.addPrimitive(operator.add, 2) #Suma
+pset.addPrimitive(operator.sub, 2) #Resta
+pset.addPrimitive(operator.mul, 2) #Multiplicacion
 pset.addEphemeralConstant("rand101", partial(random.randint, -1, 1))
 pset.renameArguments(ARG0='x')
 pset.renameArguments(ARG1='y')
 
+
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
-
 toolbox = base.Toolbox()
-toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
+toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=3, max_=3)
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
-def evalSymbReg(individual, points):
+def evalSymbReg(individual):
     # Transform the tree expression in a callable function
     func = toolbox.compile(expr=individual)
-    
-    def evaluate_point(point):
-        x, y = point
-        return (func(x) - x**2 - x*y - y**2 )**2
+    values = (x / 10. for x in range(-10, 10))
+    values2 = (y / 10. for y in range(-10, 10))
+    # Evaluate the mean squared error between the expression
+    # and the real function : x*4 + x3 + x*2 + x
+    diff_func = lambda x, y: (func(x, y) - ((x*2) + (x*y) - (y*2))) * 2
+    diff = sum(map(diff_func, values, values2))
+    return diff,
 
-    sqerrors = (evaluate_point(point) for point in points)
-    return (math.fsum(sqerrors) / len(points),)
-
-toolbox.register("evaluate", evalSymbReg, points = [(x, y) for x in range(-10, 10) for y in range(-10, 10)])
+toolbox.register("evaluate", evalSymbReg)
 toolbox.register("select", tools.selTournament, tournsize=3)
 toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
@@ -69,6 +59,7 @@ def main():
     pop = toolbox.population(n=300)
     hof = tools.HallOfFame(1)
 
+
     stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
     stats_size = tools.Statistics(len)
     mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
@@ -82,5 +73,7 @@ def main():
     # print log
     return pop, log, hof
 
+
 if __name__ == "__main__":
     main()
+    
