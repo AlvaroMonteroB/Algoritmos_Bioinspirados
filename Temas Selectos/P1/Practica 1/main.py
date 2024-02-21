d7a903234d7e92a93aaa945a185913b66a54fef4
AlvaroMonteroB <alvaromonterob@outlook.com>
2024-02-21 15:29:06 -0600

    P1 general

--- Temas Selectos/P1/Practica 1/main.py
+++ Temas Selectos/P1/Practica 1/main.py
@@ -6,59 +6,71 @@ import numpy as np
 
 
 class pop:
-    def __init__(self,num_pop,generations,ls1,li1,ls2,li2,precision1,precision2) -> None:
+    def __init__(self,num_pop,generations,ls,li,precision,nvar) -> None:
         self.individuals=[]
         self.num_pop=num_pop
         self.generations=generations
-        self.ls1=ls1
-        self.li1=li1
-        self.ls2=ls2
-        self.li2=li2
-        self.precision1=precision1
-        self.precision2=precision2
+        self.nvar=nvar
+        self.ls=list()
+        self.li=[]
+        self.precision=[]
+        for i in range(nvar):
+            self.ls.append(ls[i])
+            
+        for i in range(nvar):
+            self.li.append(li[i])
+            
+        for i in range(nvar):
+            self.precision.append(precision[i])
+            
         self.pop_init()
 
 
     def decode(self,i):
-        gen1=self.individuals[i][:self.bits1]
-        gen2=self.individuals[i][self.bits1:]
+        gen=[]
+        aux=0
+        for j in range(len(self.bits)):
+            gen.append(self.individuals[i][aux:self.bits[j]+aux])
+            aux+=self.bits[j]
         Xint=0
-        Yint=0
-        #Binario a entero
-        for k in range(len(gen1)):
-            j=-1-k
-            Xint+=gen1[k]*(2**(self.bits1+j))
+        real=[]
         
-        Xreal=self.li1+((Xint*(self.ls1-self.li1))/((2**self.bits1)-1))
+        #Binario a entero
+        for l in range(self.nvar):#Numero de variables/genes
+            Xint=0
+            for k in range(len(gen[l])):
+                j=-1-k
+                Xint+=gen[l][k]*(2**(self.bits[l]+j))
+            real.append(self.li[l] +((Xint*(self.ls[l]-self.li[l]))/((2**self.bits[l])-1)))
             
-        for k in range(len(gen2)):
-            j=-1-k
-            Yint+=gen2[k]*(2**(self.bits2+j))
-        Yreal=self.li2+((Yint*(self.ls2-self.li2))/((2**self.bits2)-1))
         #Imprimir Individuos codificados y decodificados
         print("Individuo "+str(i))
-        print("Codificado: "+str(gen1)+" Real: "+str(Xreal))
-        print("Codificado: "+str(gen2)+" Real: "+str(Yreal))
-        return (Xreal,Yreal)
+        print("Codificado: "+str(gen[0])+" Real: "+str(real[0]))
+        print("Codificado: "+str(gen[1])+" Real: "+str(real[1]))
+        return real
         
       
         
     def pop_init(self):
-        self.bits1=int(math.log((self.ls1-self.li1)*10**self.precision1,2)+.9)
-        self.bits2=int(math.log((self.ls2-self.li2)*10**self.precision2,2)+.9)
+        self.bits=[]
+        for i in range(self.nvar):
+            self.bits.append(int(math.log((self.ls[i]-self.li[i])*10**self.precision[i],2)+.9))
+            
         for i in range(self.num_pop):#for para iterar en los individuos
-            individual=np.random.randint(2,size=(self.bits1+self.bits2))
+            individual=np.random.randint(2,size=(sum(self.bits)))
             self.individuals.append(individual)
             
     
     def evaluate_all(self):
         for i in range(len(self.individuals)):
-            x,y=self.decode(i)
-            print("F("+str(x)+","+str(y)+")= "+str(self.obj_funct(x,y)))
+            values=self.decode(i)
+            print("F("+str(values[0])+","+str(values[1])+")= "+str(self.obj_funct(values)))
             
             
             
-    def obj_funct(self,x,y):
+    def obj_funct(self,real):
+        x=real[0]
+        y=real[1]
         return ((1-x)**2 + (100-y)**2)
     
     
@@ -66,6 +78,6 @@ class pop:
         self
     
     
-    
-Poblacion=pop(5,10,2,-2,2,-2,2,2)
+#Se tienen que poner juntos los limites superiores e inferiores, en la misma tupla
+Poblacion=pop(5,10,(2,2),(-2,-2),(2,2),2)
 Poblacion.evaluate_all()
